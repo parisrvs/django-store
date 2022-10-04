d69403f1feb8456e87e1b37a124bee04a1034fd3
Parijat Srivastava <parisrvs@hotmail.com>
2022-10-04 13:30:52 +0530

    commented out database update

--- testapp/views.py
+++ testapp/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import database
-from store.models import OrderItem, Product, Variation
-from django.db.models import Count, F, Value
+
 
 def homepage(request):
     return render(
@@ -13,24 +12,24 @@ def homepage(request):
 
 
 def database_update(request):
-    database.UPDATE_DISCOUNT()
-    database.UPLOAD_CATEGORIES()
-    database.UPLOAD_PRODUCTS()
-    database.UPLOAD_TAGS()
-    database.UPDATE_PROUCTS()
-    database.UPDATE_KEYVALUE()
-    database.UPDATE_VARIATIONS()
-    database.inventory()
+    # database.UPDATE_DISCOUNT()
+    # database.UPLOAD_CATEGORIES()
+    # database.UPLOAD_PRODUCTS()
+    # database.UPLOAD_TAGS()
+    # database.UPDATE_PROUCTS()
+    # database.UPDATE_KEYVALUE()
+    # database.UPDATE_VARIATIONS()
+    # database.inventory()
     return HttpResponseRedirect(reverse("store:homepage"))
 
 
 
 def clear_database_but_images(request):
-    database.clear_all_but_images()
+    # database.clear_all_but_images()
     return HttpResponseRedirect(reverse("store:homepage"))
 
 
 
 def clear_database_images(request):
-    database.clear_images()
+    # database.clear_images()
     return HttpResponseRedirect(reverse("store:homepage"))
